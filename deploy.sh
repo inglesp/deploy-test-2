a1f8203ab537ca696c15602a993bbc05707885dc
Peter Inglesby <peter.inglesby@gmail.com>
2015-06-07 14:19:12 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,6 @@ if [[ $TRAVIS = "true" ]]; then
 	if [[ $TRAVIS_BRANCH = "master" && $TRAVIS_PULL_REQUEST = "false" ]]; then
 		echo "Deploying!"
 
-		echo "Last log:"
-		git log -n 1 --parents
-
 		# Set up credentials for pushing to GitHub.  $GH_TOKEN is
 		# configured via Travis web UI.
 		git config credential.helper "store --file=.git/credentials"
@@ -19,17 +16,23 @@ if [[ $TRAVIS = "true" ]]; then
 		# been updated if required when pre-flight-checks.sh ran.  If
 		# the output directory is already up to date then no new commit
 		# will be made.
+		echo "Last log:"
+		git log -n 1 --parents
+
+		echo "Checking out master"
+		git checkout master
+
+		echo "Last log:"
+		git log -n 1 --parents
+
 		echo "Committing"
 		git commit -a -m "Travis auto-commit.  Built latest changes."
 
 		echo "Last log"
 		git log -n 1 --parents
 
-		# git push https://inglesp@github.com/inglesp/deploy-test-2 master
-
-		echo "Checking out master"
-		git checkout master
-		git log -n 3 --parents
+		echo "Pushing"
+		git push https://inglesp@github.com/inglesp/deploy-test-2 master
 
 		# Push output directory to gh-pages branch on GitHub.
 		# git subtree push --prefix output https://inglesp@github.com/inglesp/deploy-test-2 gh-pages
